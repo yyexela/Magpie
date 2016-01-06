3e805cdb0281dac4d9cabb672c1fa26a2238704c
Tim Padjen <tpadjen@gmail.com>
2016-01-06 08:05:53 -0700

    Reformat source

--- src/Magpie2.java
+++ src/Magpie2.java
@@ -1,26 +1,27 @@
 /**
- * A program to carry on conversations with a human user.
- * This is the initial version that:  
- * <ul><li>
- *       Uses indexOf to find strings
- * </li><li>
- * 		    Handles responding to simple words and phrases 
- * </li></ul>
+ * A program to carry on conversations with a human user. This is the initial
+ * version that:
+ * <ul>
+ * <li>
+ * Uses indexOf to find strings</li>
+ * <li>
+ * Handles responding to simple words and phrases</li>
+ * </ul>
  * This version uses a nested if to handle default responses.
+ * 
  * @author Laurie White
  * @version April 2012
  */
-public class Magpie2
-{
+public class Magpie2 {
 	/**
-	 * Get a default greeting 	
+	 * Get a default greeting
+	 * 
 	 * @return a greeting
 	 */
-	public String getGreeting()
-	{
+	public String getGreeting() {
 		return "Hello, let's talk.";
 	}
-	
+
 	/**
 	 * Gives a response to a user statement
 	 * 
@@ -28,22 +29,16 @@ public class Magpie2
 	 *            the user statement
 	 * @return a response based on the rules given
 	 */
-	public String getResponse(String statement)
-	{
+	public String getResponse(String statement) {
 		String response = "";
-		if (statement.indexOf("no") >= 0)
-		{
+		if (statement.indexOf("no") >= 0) {
 			response = "Why so negative?";
-		}
-		else if (statement.indexOf("mother") >= 0
+		} else if (statement.indexOf("mother") >= 0
 				|| statement.indexOf("father") >= 0
 				|| statement.indexOf("sister") >= 0
-				|| statement.indexOf("brother") >= 0)
-		{
+				|| statement.indexOf("brother") >= 0) {
 			response = "Tell me more about your family.";
-		}
-		else
-		{
+		} else {
 			response = getRandomResponse();
 		}
 		return response;
@@ -51,29 +46,22 @@ public class Magpie2
 
 	/**
 	 * Pick a default response to use if nothing else fits.
+	 * 
 	 * @return a non-committal string
 	 */
-	private String getRandomResponse()
-	{
+	private String getRandomResponse() {
 		final int NUMBER_OF_RESPONSES = 4;
 		double r = Math.random();
-		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
-		
-		if (whichResponse == 0)
-		{
+
+		if (whichResponse == 0) {
 			response = "Interesting, tell me more.";
-		}
-		else if (whichResponse == 1)
-		{
+		} else if (whichResponse == 1) {
 			response = "Hmmm.";
-		}
-		else if (whichResponse == 2)
-		{
+		} else if (whichResponse == 2) {
 			response = "Do you really think so?";
-		}
-		else if (whichResponse == 3)
-		{
+		} else if (whichResponse == 3) {
 			response = "You don't say.";
 		}
 
