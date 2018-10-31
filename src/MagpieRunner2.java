3c143a36c474f514852ef549d1b0260d3de4cdcb
Alexey Yermakov <alexey@epmaki.net>
2018-10-31 11:21:09 -0600

    MagpieRunner2.java fixed 'bye' command

--- src/MagpieRunner2.java
+++ src/MagpieRunner2.java
@@ -18,7 +18,7 @@ public class MagpieRunner2 {
 		Scanner in = new Scanner(System.in);
 		String statement = in.nextLine();
 
-		while (!statement.toLowerCase().equals("Bye")) {
+		while (!statement.toLowerCase().equals("bye")) {
 			System.out.println(maggie.getResponse(statement));
 			statement = in.nextLine();
 		}
